491c79b9705ea541e6719563fe8de684d56dace6
ErikaMBarroso <ermbarroso@gmail.com>
2025-06-08 12:18:58 -0300

    codigo comentado

--- eventos.js
+++ eventos.js
@@ -1,6 +1,9 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
-  const button = $(event.relatedTarget);            
-  const imagePath = button.data('img');              
-  const modal = $(this);                             
-  modal.find('#modal-image').attr('src', imagePath); 
+$('#exampleModal').on('show.bs.modal', function (event) {//$('#exampleModal') seleciona o elemento que tem id examplemodal
+                                                        //show.bs.modal faz com que o evento aconteça logo antes do modal ser aberto
+                                                        //function(event) é o que vai ser executado quando o evento acontecer
+  const button = $(event.relatedTarget); //event.relatedtarget é o button, esse dado é guardado na constant button           
+  const imagePath = button.data('img');  //lê o valor do atributo data-img do botão que abriu o modal           
+  const modal = $(this); //this se refere ao modal, esse dado é guardado na constante modal                          
+  modal.find('#modal-image').attr('src', imagePath); //dentro do modal, o código procura um elemento com ID modal-image
+                                                    //depois, ele altera o atributo src dessa imagem para o valor de imagePath, para que mostre a imagem certa
 });
--- login.js
+++ login.js
@@ -1,16 +1,22 @@
 (() => {
-  'use strict'
+  'use strict'//faz com que o código dê erro se houver variável não declaradas
+              //garante que o código fique mais limpo
 
-  const forms = document.querySelectorAll('.needs-validation')
-
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
+  const forms = document.querySelectorAll('.needs-validation')//seleciona todos os elementos de classe "needs-validation"
+                                                              //e guarda esse dado em uma constante "forms"
+  
+  Array.from(forms).forEach(form => {//array.from(forms) transforma forms em um array para poder usar .foreach
+                                     //.foreach executa a função para cada item do array
+                                     //form é a variável temporária que representa o formulário atual no loop
+      form.addEventListener('submit', event => {//adiciona um evento para forms, sempre que o usuário tentar enviar
+                                                //o formulário, essa função será executada
+      if (!form.checkValidity()) {//diz que se o formulário estiver inválido
+        event.preventDefault()    //deve impedir o envio do formulário
+        event.stopPropagation()   //interrompe alguma coisa para impedir conflitos
       }
 
-      form.classList.add('was-validated')
+      form.classList.add('was-validated')//adiciona a classe "was validated" à tag formulário
+                                        //isso faz com que as caixas de preenchimento fiquem verdes
     }, false)
   })
 })()  
